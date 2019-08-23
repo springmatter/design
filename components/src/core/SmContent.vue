2020199a853950c26c7b1f098b2155f57528cec3
mdkq <marcusdk@icloud.com>
2019-08-23 14:02:59 -0400

    beginning refactor, almost done with core

---
+++ components/src/core/SmContent.vue
@@ -0,0 +1,14 @@
+<template>
+  <router-view class="SmContent"></router-view>
+</template>
+
+<script>
+export default {
+  name: "SmContent"
+}
+</script>
+
+<style scoped>
+.SmContent {
+}
+</style>
---
+++ components/src/core/SmNav.vue
@@ -0,0 +1,87 @@
+<template>
+    <nav id="nav-top">
+      <template>
+        <SmLink to="/" title="Home">
+          <SmTitle>{{ title }}</SmTitle>
+        </SmLink>
+        <!-- <router-link -->
+        <!--   class="black" -->
+        <!--   v-for="(bc, index) in breadcrumbs" -->
+        <!--   :key="index" -->
+        <!--   :to="bc.href" -->
+        <!-- > -->
+        <!--   / {{ bc.printed }} -->
+        <!-- </router-link> -->
+        <!-- <userDropdown class="mla p3" /> -->
+      </template>
+    </nav>
+    <nav id="nav-side" :class="{ hidden: collapse }">
+      <template>
+        <SmLink v-for="link in links" :key="link.route" :to="link.route" class="side-link">
+          <SmIcon name="link.name" />
+        </SmLink>
+      </template>
+    </nav>
+</template>
+
+<script>
+export default {
+  name: "SmNav",
+  props: {
+    collapse: {
+      type: Boolean,
+      required: false,
+      default: false
+    },
+    title: {
+      type: String,
+      required: false,
+      default: ""
+    },
+    links: {
+      type: Array,
+      required: false,
+      default: [],
+      validator: function (value) {
+        if (value.length > 0) {
+          return "name" in value[0] && "route" in value[0];
+        } else {
+          return true;
+        }
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+nav#nav-top {
+  grid-area: topNav;
+  height: var(--sp7);
+  padding-left: var(--sp1);
+  display: flex;
+  align-items: center;
+  background: var(--white);
+  border-bottom: 1px solid var(--gray1);
+}
+
+nav#nav-side {
+  grid-area: sideNav;
+  width: var(--sp7);
+  display: flex;
+  align-items: center;
+  flex-flow: column;
+  background: var(--white);
+  border-right: 1px solid var(--gray1);
+  border-top: 1px solid white;
+  margin-top: -1px;
+  padding: var(--sp2) 0;
+}
+
+.side-link {
+  padding: var(--sp1);
+  width: 28px;
+  height: 28px;
+  margin-bottom: var(--sp2);
+}
+</style>

372580bdbef6c656d8a2ff85e6acfc0e7e932e79
mdkq <marcusdk@icloud.com>
2019-09-19 10:50:37 -0400

    adding SmBlankLayout

---
+++ components/src/core/SmBlankLayout.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="SmBlankLayout">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SmBlankLayout",
+  slotted: true
+}
+</script>
+
+<style scoped>
+.SmBlankLayout {
+  grid-area: content;
+}
+</style>
+

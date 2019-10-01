e03a7553f324368b53e746165d6402d12e57dc48
mdkq <marcusdk@icloud.com>
2019-10-01 12:16:25 -0400

    fixing nav prop

--- components/src/core/SmApp.vue
+++ components/src/core/SmApp.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="SmApp">
     <template v-if="!blank">
-      <SmNav type="top" :title="title" />
-      <SmNav type="side" :links="links" />
+      <SmNav kind="top" :title="title" />
+      <SmNav kind="side" :links="links" />
       <router-view></router-view>
     </template>
     <template v-if="blank">
@@ -19,17 +19,19 @@ export default {
     blank: {
       type: Boolean,
       required: false,
-      description: "Removes all default children of SmApp but maintains its grid structure. This is useful if you want to keep the app layout but tweak the nav bars. Check out the source for this website on github for an example."
+      description:
+        "Removes all default children of SmApp but maintains its grid structure. This is useful if you want to keep the app layout but tweak the nav bars. Check out the source for this website on github for an example."
     },
     title: {
       type: String,
       required: false,
-      description: "This is the title of your app that will appear in the top left on the nav bar."
+      description:
+        "This is the title of your app that will appear in the top left on the nav bar."
     },
     links: {
       type: Array,
       required: false,
-      validator: function (value) {
+      validator: function(value) {
         // IGNORE
         if (value.length > 0) {
           return "name" in value[0] && "route" in value[0];
@@ -37,10 +39,11 @@ export default {
           return true;
         }
       },
-      description: "These are the links that will appear in the side nav bar. It needs to be an array of objects with keys 'name' (the name of the icon that will represent your link) and 'route' (the route you'd like to go to in the form '/route-here'). Example: [ { name: 'map', route: '/maps' }, { name: 'archive', route: '/library' }, ... ]"
+      description:
+        "These are the links that will appear in the side nav bar. It needs to be an array of objects with keys 'name' (the name of the icon that will represent your link) and 'route' (the route you'd like to go to in the form '/route-here'). Example: [ { name: 'map', route: '/maps' }, { name: 'archive', route: '/library' }, ... ]"
     }
   }
-}
+};
 </script>
 
 <style scoped>

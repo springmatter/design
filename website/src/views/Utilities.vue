f9698c0267ce14e8297b5539cf3f3ee31105b387
mdkq <marcusdk@icloud.com>
2019-10-07 14:20:57 -0400

    updating documentation

--- website/src/views/Utilities.vue
+++ website/src/views/Utilities.vue
@@ -1,4 +1,12 @@
 <template>
   <SmTextLayout>
+    <h1>Utilities</h1>
+    <p>
+      We provide a set of utility classes that are generated from a custom
+      configuration of <a href="https://tailwindcss.com/">tailwindcss</a>. Our
+      classes differ slightly from the defaults at tailwind, but are mostly the
+      same. We will be documenting our exact classes in the future, but for now
+      you can mostly rely on the tailwind documentation.
+    </p>
   </SmTextLayout>
 </template>

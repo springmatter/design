f04f21794f2d582af27724c620441e56bf94bd0f
mdkq <marcusdk@icloud.com>
2019-10-07 14:12:00 -0400

    moving website and updating docs

--- website/src/main.js
+++ website/src/main.js
@@ -2,17 +2,16 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 
-import VueMarkdown from "vue-markdown";
 import ComponentSpec from "./components/ComponentSpec.vue";
-import components from "../src/index.js";
+import components from "../../components/src/index.js";
+import "./style.css";
 
-var comps = {}
+var comps = {};
 components.forEach(comp => {
-  Vue.component(comp.name, comp); 
+  Vue.component(comp.name, comp);
   comps[comp.name] = comp;
 });
 
-Vue.component("VueMarkdown", VueMarkdown);
 Vue.component("ComponentSpec", ComponentSpec);
 
 Vue.config.productionTip = false;

bc0f736c415527d2a5df2b74aeaebb701e8920ca
mdkq <marcusdk@icloud.com>
2019-10-16 12:30:52 -0400

    adding sandbox

---
+++ components/sandbox/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    TEST
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
---
+++ components/sandbox/main.js
@@ -0,0 +1,14 @@
+import Vue from "vue";
+import App from "./App.vue";
+
+import components from "../../components/src/index.js";
+
+components.forEach(comp => {
+  Vue.component(comp.name, comp);
+});
+
+Vue.config.productionTip = false;
+
+new Vue({
+  render: h => h(App)
+}).$mount("#app");

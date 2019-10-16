8d6c7725892963b9c0c5460290d87c1aed1b81d9
mdkq <marcusdk@icloud.com>
2019-10-16 12:30:25 -0400

    stripping down

--- components/tailwind.config.js
+++ components/tailwind.config.js
@@ -1,76 +1,200 @@
 module.exports = {
   theme: {
     colors: {
-      transparent: 'transparent',
-
-      black: '#000',
-      white: '#fff',
-
+      transparent: "transparent",
+      black: "#000",
+      white: "#fff",
       gray: {
-        1: '#f3f3f3',
-        2: '#dcdcdc',
-        3: '#bebebe',
-        4: '#a4a4a4',
-        5: '#8c8c8c',
-        6: '#6f6f6f',
-        7: '#565656',
-        8: '#3d3d3d',
-        9: '#282828',
-        10: '#171717',
+        1: "#F8F9FA",
+        2: "#DDE2E5",
+        3: "#ACB5BD",
+        4: "#495057"
       },
-      red: {
-        light: '#E0464F',
-        default: '#DA1E28',
-        dark: '#B31921'
+      primary: {
+        default: "#0038FF",
+        hover: "#0032E3"
       },
-      orange: {
-        light: '#FC9346',
-        default: '#FC7B1E',
-        dark: '#CF6519'
+      secondary: {
+        default: "#141618",
+        hover: "#000"
       },
-      yellow: {
-        light: '#FDD95D',
-        default: '#FDD13A',
-        dark: '#D0AC30'
-      },
-      green: {
-        light: '#4BB269',
-        default: '#24A148',
-        dark: '#1E843B'
-      },
-      blue: {
-        light: '#2E7EFF',
-        default: '#0062FF',
-        dark: '#0051D1'
-      },
-      purple: {
-        light: '#9F61FC',
-        default: '#8A3FFC',
-        dark: '#7134CF'
-      },
+      error: "#DA1E28",
+      success: "#1D833B",
+      highlight: "#FFE55E"
     },
     spacing: {
-      px: '1px',
-      '0': '0',
-      '1': '0.25rem',
-      '2': '0.5rem',
-      '3': '0.75rem',
-      '4': '1rem',
-      '5': '1.5rem',
-      '6': '2rem',
-      '7': '4rem',
-      '8': '6rem',
-      '9': '10rem',
-      '10': '16rem',
+      px: "1px",
+      "0": "0",
+      "1": "4px",
+      "2": "8px",
+      "3": "12px",
+      "4": "16px",
+      "5": "24px",
+      "6": "32px",
+      "7": "40px",
+      "8": "48px",
+      "9": "56px",
+      "10": "64px"
     },
-    fontWeight: {
-      normal: '400',
-      bold: '700'
+    backgroundColor: theme => theme("colors"),
+    borderColor: theme => ({
+      ...theme("colors"),
+      default: theme("colors.secondary", "currentColor")
+    }),
+    borderWidth: {
+      default: "1px",
+      "0": "0",
+      "2": "2px"
     },
+    cursor: {
+      auto: "auto",
+      default: "default",
+      pointer: "pointer",
+      wait: "wait",
+      text: "text",
+      move: "move",
+      "not-allowed": "not-allowed"
+    },
+    flex: {
+      "1": "1 1 0%",
+      auto: "1 1 auto",
+      initial: "0 1 auto",
+      none: "none"
+    },
+    flexGrow: {
+      "0": "0",
+      default: "1"
+    },
+    flexShrink: {
+      "0": "0",
+      default: "1"
+    },
+    height: theme => ({
+      auto: "auto",
+      ...theme("spacing"),
+      full: "100%",
+      screen: "100vh"
+    }),
+    inset: {
+      "0": "0",
+      auto: "auto"
+    },
+    listStyleType: {
+      none: "none",
+      disc: "disc",
+      decimal: "decimal"
+    },
+    margin: (theme, { negative }) => ({
+      auto: "auto",
+      ...theme("spacing"),
+      ...negative(theme("spacing"))
+    }),
+    opacity: {
+      "0": "0",
+      "25": "0.25",
+      "50": "0.5",
+      "75": "0.75",
+      "100": "1"
+    },
+    order: {
+      first: "-9999",
+      last: "9999",
+      none: "0",
+      "1": "1",
+      "2": "2",
+      "3": "3",
+      "4": "4",
+      "5": "5",
+      "6": "6",
+      "7": "7",
+      "8": "8",
+      "9": "9",
+      "10": "10",
+      "11": "11",
+      "12": "12"
+    },
+    padding: theme => theme("spacing"),
+    textColor: theme => theme("colors"),
+    width: theme => ({
+      auto: "auto",
+      ...theme("spacing"),
+      "1/2": "50%",
+      "1/3": "33.333333%",
+      "2/3": "66.666667%",
+      "1/4": "25%",
+      "2/4": "50%",
+      "3/4": "75%",
+      "1/5": "20%",
+      "2/5": "40%",
+      "3/5": "60%",
+      "4/5": "80%",
+      full: "100%"
+    }),
+    zIndex: {
+      auto: "auto",
+      "0": "0",
+      "10": "10",
+      "20": "20",
+      "30": "30",
+      "40": "40",
+      "50": "50"
+    }
   },
   variants: {
-    bg: ["disabled"],
-    text: ["disabled"],
+    backgroundColor: ["hover", "active", "disabled"],
+    borderColor: ["hover", "active", "disabled"],
+    borderWidth: [],
+    cursor: [],
+    flex: [],
+    flexGrow: [],
+    flexShrink: [],
+    height: [],
+    inset: [],
+    listStyleType: [],
+    margin: [],
+    opacity: [],
+    order: [],
+    padding: [],
+    textColor: ["hover", "active", "visited", "disabled"],
+    width: [],
+    zIndex: []
   },
-  plugins: [],
-}
+  corePlugins: {
+    container: false,
+    float: false,
+    objectFit: false,
+    objectPosition: false,
+    visibility: false,
+    fontFamily: false,
+    fontSize: false,
+    fontStyle: false,
+    fontWeight: false,
+    fontSmoothing: false,
+    letterSpacing: false,
+    lineHeight: false,
+    listStylePosition: false,
+    placeholderColor: false,
+    verticalAlign: false,
+    whitespace: false,
+    wordBreak: false,
+    backgroundAttachment: false,
+    backgroundPosition: false,
+    backgroundRepeat: false,
+    backgroundSize: false,
+    borderCollapse: false,
+    tableLayout: false,
+    boxShadow: false,
+    appearance: false,
+    outline: false,
+    fill: false,
+    stroke: false,
+    borderStyle: false,
+    borderRadius: false,
+    negativeMargin: false,
+    resize: false,
+    maxHeight: false,
+    maxWidth: false,
+    minHeight: false,
+    minWidth: false
+  }
+};

ac887bcf837a683e84562773d4ab2d3ea6363a76
Hannah VanWingen <hvanova@umich.edu>
2019-10-31 15:38:55 -0400

    dropdown functional for dict and string

--- components/src/components/SmDropdown.vue
+++ components/src/components/SmDropdown.vue
@@ -1,37 +1,20 @@
 <template>
   <div class="SmDropdown">
-    <div class="selection" @click="expanded? contract(): expand()">
+    <div class="selectionClass" @click="expanded? contract(): expand()">
       {{selection}}
       <SmIcon class="SmDropdownChevron" name="chevron-down" />
     </div>
     <div v-if="expanded">
       <SmSearch v-if="searchable" />
       <div
-        v-for="result in results"
+        v-for="(result, index) in results"
         :key="result"
-        class="result"
+        :class="[index == 0 ? 'topResultClass':'', index != Object.keys(results).length - 1 ? 'resultClass':'']"
         @click="selectTarget(result)"
       >{{result}}</div>
     </div>
   </div>
 </template>
-  <!-- <div class="container">
-    <div class="card">
-      <div v-if="!expanded">
-        Select a Target
-        <SmIcon class="icon" name="chevron-down" size="s" @click.native="expand()" />
-      </div>
-      <div v-if="expanded">
-        <input v-model="filter" placeholder="Search..." :keyup="filterResults()" />
-        <div
-          class="result"
-          v-for="result in results"
-          :key="result"
-          @click="selectTarget(result)"
-        >{{result}}</div>
-      </div>
-    </div>
-  </div>-->
 
 <script>
 import SmIcon from "./SmIcon.vue";
@@ -46,15 +29,35 @@ export default {
   data: function() {
     return {
       expanded: false,
-      selection: "Select...",
-      results: []
+      selection: "Select..."
     };
+  },
+  computed: {
+    results: function() {
+      if (this.targetType == "string") {
+        return this.targets;
+      }
+      // Else, use first key as a display for results.
+      let results = [];
+      for (var i = 0; i < this.targets.length; i++) {
+        let key = Object.keys(this.targets[i])[0];
+        results.push(this.targets[i][key]);
+      }
+      return results;
+    }
   },
   props: {
     targets: {
       type: Array,
       required: true,
       description: "A list of targets to select."
+    },
+    targetType: {
+      type: String,
+      required: false,
+      default: "string",
+      description:
+        "Used to determine if 'targets' is a list of dictionaries or strings."
     },
     searchable: {
       type: Boolean,
@@ -65,13 +68,11 @@ export default {
   methods: {
     expand: function() {
       this.expanded = true;
-      this.results = this.targets;
       var icon = document.getElementsByClassName("SmDropdownChevron")[0];
       icon.style.transform = "rotate(180deg)";
     },
     selectTarget(result) {
-      this.selection = result;
-      this.expanded = false;
+      this.selection = "Selected: " + result;
       this.contract();
     },
     contract: function() {
@@ -86,7 +87,7 @@ export default {
 <style scoped>
 .SmDropdown {
   border: 1px solid var(--secondary);
-  padding: 8px 16px 8px 16px;
+  padding: 8px 16px 6px 16px;
   display: inline-block;
   color: black;
   background: white;
@@ -104,16 +105,20 @@ export default {
   z-index: 1;
 }
 
-.selection {
-  color: grey;
+.selectionClass {
+  padding-bottom: 2px;
 }
 
-.result {
+.resultClass {
   border-radius: 0px;
   border-bottom: 1px solid lightgrey;
 }
 
-.result:hover {
+.topResultClass {
+  border-top: 1px solid lightgrey;
+}
+
+.resultClass:hover {
   color: var(--primary-hover);
 }
 </style>

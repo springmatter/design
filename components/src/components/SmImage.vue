30e3b6a1acfdba97e211f16a29c4aee204163caf
mdkq <marcusdk@icloud.com>
2019-11-18 12:44:12 -0500

    using outline

--- components/src/components/SmImage.vue
+++ components/src/components/SmImage.vue
@@ -1,21 +1,15 @@
 <template>
-  <figure class="SmImage">
-    <transition name="fade">
-      <img
-        :src="src"
-        @load="srcLoading = false"
-        v-show="!srcLoading && !loading"
-        class="SmImageImg"
-        :style="{
-          objectFit: objectFit,
-          objectPosition: objectPosition
-        }"
-      />
-    </transition>
-    <div class="SmImageGlow" v-if="srcLoading || loading">
-      <SmIcon name="image" class="SmImageGlowIcon" />
-    </div>
-  </figure>
+  <img
+    :src="src"
+    class="SmImage"
+    @load="srcLoading = false"
+    :class="{ SmImageLoading: srcLoading || loading }"
+    :style="{
+      outlineWidth: loading || srcLoading ? `${outlineWidth}px` : 0,
+      outlineOffset: `${-(outlineWidth + 1)}px`
+    }"
+    ref="img"
+  />
 </template>
 
 <script>
@@ -23,7 +17,8 @@ export default {
   name: "SmImage",
   data: function() {
     return {
-      srcLoading: true
+      srcLoading: true,
+      outlineWidth: 1000
     };
   },
   props: {
@@ -34,73 +29,20 @@ export default {
     loading: {
       required: false,
       type: Boolean
-    },
-    objectFit: {
-      required: false,
-      type: String,
-      default: "unset",
-      validator: function(value) {
-        return (
-          ["contain", "cover", "fill", "none", "scale-down", "unset"].indexOf(
-            value
-          ) !== -1
-        );
-      }
-    },
-    objectPosition: {
-      required: false,
-      type: String,
-      default: "unset",
-      validator: function(value) {
-        const MATCH = value.match(
-          /([0-9]*px|[0-9]*%|right|left|top|bottom|center)/g
-        );
-        return (MATCH !== null && MATCH.length === 2) || value === "unset";
-      }
     }
+  },
+  mounted: function() {
+    this.outlineWidth = this.$refs.img.offsetWidth / 2;
   }
 };
 </script>
 
 <style>
-.SmImage {
-  position: relative;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
-
-.SmImageImg {
-  width: 100%;
-  height: 100%;
-}
-
-.fade-enter {
-  opacity: 0;
-}
-
-.fade-enter-active {
-  transition: opacity 0.75s ease;
-}
-
-.SmImageGlow {
-  width: 100%;
-  height: 100%;
-  outline: 4px solid var(--gray2);
-  outline-offset: -8px;
-  animation: 0.75s infinite linear glow alternate;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
-
-.SmImageGlowIcon {
-  width: 50% !important;
-  height: 50% !important;
-  max-width: 80px !important;
-  max-height: 80px !important;
-  color: var(--gray2);
-  position: absolute;
+.SmImageLoading {
+  animation: glow 0.75s ease-in-out infinite alternate;
+  outline-color: var(--gray2);
+  outline-style: solid;
+  padding: 0px;
 }
 
 @keyframes glow {
@@ -109,7 +51,7 @@ export default {
   }
 
   to {
-    opacity: 0.75;
+    opacity: 1;
   }
 }
 </style>
